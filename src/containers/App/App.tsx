6f67c9d12f6e7a3de5a094c1aeed2f38e1249a5e
Eliran Hasin <eliran.hasin@ls-techs.com>
2019-02-20 12:26:31 +0200

    123

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,13 +2,8 @@ import { Provider } from 'mobx-react';
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import Login from '../../components/Login/MainLogin/Login';
-import { RoutesPath } from '../../consts/urlParams';
 import rootStores from '../../stores';
-import { CustomButton } from 'nofshonit-base-web-client';
-import LoginHeader from '../../components/Login/MainLogin/LoginHeader';
-import LoginWithEmail from '../../components/Login/LoginWithEmail/LoginWithEmail';
-import ForgotPassword from '../../components/Login/ForgotPassword/ForgotPassword';
+
 import LoginContainer from '../Login/LoginContainer';
 interface IProps {}
 
