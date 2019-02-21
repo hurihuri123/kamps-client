8bec74a1bcddbb43af2c8bf0a1f3763b7459e721
Gal <gal.madar@ls-techs.com>
2019-02-21 18:28:07 +0200

    CodeReview with eliran on validations....

--- src/containers/Login/LoginContainer.tsx
+++ src/containers/Login/LoginContainer.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
-import { Route } from 'react-router';
+import {Route} from 'react-router';
 import ForgotPassword from '../../components/Login/ForgotPassword/ForgotPassword';
 import LoginWithEmail from '../../components/Login/LoginWithEmail/LoginWithEmail';
 import Login from '../../components/Login/MainLogin/Login';
 import Register from '../../components/Login/Register/Register';
 import UpdatePassword from '../../components/Login/UpdatePassword/UpdatePassword';
 import WithoutEmail from '../../components/Login/WithoutEmail/WithoutEmail';
-import { RoutesPath } from '../../consts/urlParams';
-
-
+import {RoutesPath} from '../../consts/urlParams';
 
 export default class LoginContainer extends React.Component {
   render() {
     return (
-
       <React.Fragment>
         <Route path={`/${RoutesPath.login}/${RoutesPath.withOutEmail}`} exact={false} component={WithoutEmail} />
         <Route path={`/${RoutesPath.login}/${RoutesPath.updatePassword}`} exact={false} component={UpdatePassword} />
@@ -22,7 +19,6 @@ export default class LoginContainer extends React.Component {
         <Route path={`/${RoutesPath.login}/${RoutesPath.loginWithEmail}`} exact={false} component={LoginWithEmail} />
         <Route path={`/${RoutesPath.login}`} exact={true} component={Login} />
       </React.Fragment>
-
     )
   }
 }
